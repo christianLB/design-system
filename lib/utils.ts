cda474adec2eaf76d351579d081b4f41467bfb7c
christian.aguero.1983 <christian.aguero.1983@gmail.com>
2025-04-23 08:41:20 +0000

    fix build

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
 import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "../node_modules/tailwind-merge"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
